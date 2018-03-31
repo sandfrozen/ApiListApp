beeaba8150136e21d9fd1ca6ae9abb09ebbd198b
Tomek Buslowski <tbuslowski@gmail.com>
2018-03-31 15:24:51 +0200

    Loading News API is working, but need prettifying

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
 import React from 'react';
-import { View, Text, Button, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Button, Image, TouchableOpacity, ListView, FlatList, Alert } from 'react-native';
 import { List, ListItem } from 'react-native-elements'
 import { StackNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { ScrollView } from 'react-native';
 
 class HomeScreen extends React.Component {
-  static navigationOptions = ({ navigation, navigationOptions }) => {
-    const { params } = navigation.state || {};
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
+      link: 'https://newsapi.org/v2/top-headlines?country=pl&apiKey=bedefbb1d71346c2a2795c2113f469fd',
+    }
+  }
+
+  componentDidMount() {
+    // this.loadDataFromNewsAPI()
+  }
+
+  componentWillMount() {
+    this.props.navigation.setParams({ loadData: this.loadDataFromNewsAPI })
+  }
+
+  loadDataFromNewsAPI = () => {
+    const NewsAPI = require('newsapi');
+    const newsapi = new NewsAPI('bedefbb1d71346c2a2795c2113f469fd');
+    newsapi.v2.topHeadlines({
+      country: 'pl'
+    }).then(response => {
+      console.log(response);
+
+      if (response["status"] == "ok" && response["totalResults"] != "0") {
+        console.log("znaleziono")
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(response.articles),
+        })
+      } else if(response["totalResults"] == "0") {
+        console.log("nie znaleziono")
+        Alert.alert(
+          'Articles not found',
+          'Recieved 0 items.',
+          [
+            {text: 'OK'},
+          ],
+          { cancelable: false }
+        )
+      }
+    });
+  }
+
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    // const { params } = navigation.state || {};
+    const params = navigation.state.params || {};
+    const NewsAPI = require('newsapi');
+    const newsapi = new NewsAPI('bedefbb1d71346c2a2795c2113f469fd');
 
     return {
       title: "Home",
@@ -21,13 +68,58 @@ class HomeScreen extends React.Component {
         <LogoTitle onPress={() => navigation.navigate('MyModal')} />
       ),
       headerRight: (
-        <RefreshIcon onPress={() => navigation.navigate('MyModal')} />
+        <RefreshIcon onPress={params.loadData} />
       ),
     }
   };
 
+  loadDataFromNewsAPI2() {
+    // fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=bedefbb1d71346c2a2795c2113f469fd')
+    // .then( (response) => response.json())
+    // .then( (responseJson) => {
+    //   console.log(responseJson)
+    //   data = responseJson["articles"]
+    //   if( responseJson["status"] == "ok" && responseJson["totalResults"] != "0") {
+    //     console.log("znaleziono")
+    //     this.setState({
+    //       dataSource: this.state.dataSource.cloneWithRows(data),
+    //     })
+    //   } else {
+    //     console.log("nie znaleziono")
+    //   }
+
+    // })
+    // .catch( (error) => {
+    //   console.error(error)
+    // })
+  }
+
+  renderRow(rowData, sectionID) {
+    return (
+      <ListItem
+        roundAvatar
+        key={sectionID}
+        title={rowData.name}
+        subtitle={rowData.subtitle}
+        avatar={{ uri: rowData.avatar_url }}
+      />
+    )
+  }
+
   render() {
-    const list = [
+    const list2 = [
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+    ]
+    const list3 = [
       {
         title: 'Appointments',
         icon: 'av-timer'
@@ -37,20 +129,20 @@ class HomeScreen extends React.Component {
         icon: 'flight-takeoff'
       },
       {
-        title: 'Appointments',
-        icon: 'av-timer'
+        title: 'Reservations',
+        icon: 'hotel'
       },
       {
-        title: 'Trips',
-        icon: 'flight-takeoff'
+        title: 'Cars',
+        icon: 'directions-car'
       },
       {
-        title: 'Appointments',
-        icon: 'av-timer'
+        title: 'Ships',
+        icon: 'directions-boat'
       },
       {
-        title: 'Trips',
-        icon: 'flight-takeoff'
+        title: 'Best offers',
+        icon: 'local-offer'
       },
     ]
     return (
@@ -70,15 +162,42 @@ class HomeScreen extends React.Component {
           title="Update the title"
           onPress={() => this.props.navigation.setParams({ title: 'Updated!' })}
         />
+        <Button
+          title="Load data"
+          onPress={() => this.loadDataFromNewsAPI()}
+        />
         <ScrollView style={{ width: "100%" }}>
-          <List containerStyle={{ marginBottom: 20 }}>
+
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) =>
+              <View><Text>{rowData.title}</Text></View>
+            }
+          />
+
+          <List>
+            <ListItem
+              roundAvatar
+              title='Limited supply! Its like digital gold!'
+              subtitle={
+                <View style={styles.subtitleView}>
+                  <Image source={require('./fsv.jpg')} style={styles.ratingImage} />
+                  <Text style={styles.ratingText}>5 months ago</Text>
+                </View>
+              }
+              avatar={require('./fsv.jpg')}
+            />
+          </List>
+
+          <List containerSstyle={{ marginBottom: 20 }}>
             {
-              list.map((l, i) => (
+              list2.map((l, i) => (
                 <ListItem
-                  roundAvatar
+                  style={{ height: 100 }}
                   avatar={{ uri: l.avatar_url }}
                   key={i}
                   title={l.name}
+
                 />
               ))
             }
@@ -86,11 +205,19 @@ class HomeScreen extends React.Component {
 
           <List>
             {
-              list.map((item, i) => (
+              list3.map((item, i) => (
                 <ListItem
                   key={i}
                   title={item.title}
                   leftIcon={{ name: item.icon }}
+                  onPress={() => {
+                    /* 1. Navigate to the Details route with params */
+                    var t = item.title
+                    this.props.navigation.navigate('Details', {
+                      itemId: { i },
+                      otherParam: t,
+                    });
+                  }}
                 />
               ))
             }
@@ -101,9 +228,25 @@ class HomeScreen extends React.Component {
   }
 }
 
+const styles = {
+  subtitleView: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 5
+  },
+  ratingImage: {
+    height: 19.21,
+    width: 100
+  },
+  ratingText: {
+    paddingLeft: 10,
+    color: 'grey'
+  }
+}
+
 class DetailsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
-    const { params } = navigation.state;
+    const params = navigation.state.params;
 
     return {
       title: params ? params.otherParam : 'A Nested Details Screen',
@@ -212,7 +355,7 @@ const RootStack = StackNavigator(
     initialRouteName: 'Main',
     mode: 'modal',
     headerMode: 'none',
-  }
+  },
 );
 
 export default class App extends React.Component {
